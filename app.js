22026abcad7b80e2fc0e8f2be6e09d605e85442b
medinenebiyeva <medinenebiyeva1@gmail.com>
2024-06-26 17:58:35 +0400

    kim

---
+++ app.js
@@ -0,0 +1,10 @@
+let year = prompt("ili daxil edin");
+if(year % 4 === 0){
+    console.log("bu uzun ildir")
+}else if(year % 100 === 0){
+    console.log("bu uzun ildir")
+}else if(year % 400 === 0){
+    console.log("bu uzun il deyil")
+}else{
+    console.log("bu uzun il deyil")
+}
